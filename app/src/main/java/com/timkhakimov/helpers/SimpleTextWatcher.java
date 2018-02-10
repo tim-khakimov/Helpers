7d4425e641270dea08f5200da76a020bd3bb603e
Timur Khakimov <tkh1511@gmail.com>
2018-02-10 15:26:58 +0300

    [+] SimpleTextWatcher

---
+++ app/src/main/java/com/timkhakimov/helpers/SimpleTextWatcher.java
@@ -0,0 +1,19 @@
+package com.timkhakimov.helpers;
+
+import android.text.TextWatcher;
+
+/**
+ * Created by Timur Khakimov on 10.02.2018.
+ */
+
+public abstract class SimpleTextWatcher implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+}
