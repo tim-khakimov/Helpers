4166b86ffe083bf0469fe96dcd4213b54617b3d0
Timur Khakimov <tkh1511@gmail.com>
2018-02-10 15:28:04 +0300

    [+] CustomPatternTextWatcher

---
+++ app/src/main/java/com/timkhakimov/helpers/CustomPatternTextWatcher.java
@@ -0,0 +1,55 @@
+package com.timkhakimov.helpers;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+/**
+ * Created by Timur Khakimov on 10.02.2018.
+ */
+
+public abstract class CustomPatternTextWatcher implements TextWatcher {
+
+    protected EditText editText;
+    protected boolean backspacingFlag = false;
+    protected boolean editedFlag = false;
+    protected int cursorComplement;
+
+    public CustomPatternTextWatcher(EditText editText) {
+        this.editText = editText;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        cursorComplement = s.length()- editText.getSelectionStart();
+        if (count > after) {
+            backspacingFlag = true;
+        } else {
+            backspacingFlag = false;
+        }
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if(!editedFlag){
+            if(!backspacingFlag) {
+                editedFlag = true;
+                editText.setText(getFormattedText(s.toString()));
+                int selection = editText.getText().length() - cursorComplement + 1;
+                if(selection>editText.getText().length()){
+                    selection = editText.getText().length();
+                }
+                editText.setSelection(selection);
+            }
+        } else {
+            editedFlag = false;
+        }
+    }
+
+    protected abstract String getFormattedText(String editedText);
+}
